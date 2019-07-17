98a93755e210005dcea4f0ac7cb85d5775b5b48d
Dongfang <1136005348@qq.com>
2019-07-17 11:19:40 +0800

    debug_v1
    每个任务完成了单独的页面显示流程，测试用按钮和背景色。

---
+++ wish/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<wish-task0 />
+	</div>
+</template>
+
+<script>
+	import WishTask0 from './views/Task0.vue'
+	//left='100px' top='200px' width='100px' height='200px' value='第一条' :items="['第一条', '第二条']"
+	export default {
+		name: 'app',
+		components: {
+			WishTask0
+		}
+	}
+</script>
+
+<style>
+	* {
+		margin: 0px;
+		padding: 0px;
+	}
+
+	#app {
+		font-family: '微软雅黑', Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
